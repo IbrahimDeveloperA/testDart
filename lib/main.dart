9b603d939986aa54032bd237c549d4856f61e36b
king <ibrahim.kasymov@gmail.com>
2023-09-11 16:08:50 +0600

    img.

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,45 @@ class FlutterTutorialApp extends StatelessWidget {
             backgroundColor: Colors.blue,
           ),
           body: Container(
-            color: Colors.blue,
-            margin: EdgeInsets.all(20),
-            padding: EdgeInsets.all(50),
-            transform: Matrix4.rotationZ(-0.1),
-            child: Text(
-              "ololol",
-              style: TextStyle(fontSize: 120.0, color: Colors.black),
+            child: Container(
+              margin: EdgeInsets.all(100),
+              padding: EdgeInsets.all(100),
+              child: Text(
+                "",
+                style: TextStyle(fontSize: 26.0, color: Colors.black),
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.green,
+                  //    borderRadius: BorderRadius.all(Radius.circular(200))
+                  //  borderRadius: BorderRadius.circular(20)
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    width: 15,
+                    color: Colors.pink,
+                  ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.red,
+                  spreadRadius: 10, // количестыо тени
+                  blurRadius: 10, // контролирует четкость тени
+                  offset: Offset(3,3) //контроль тени
+                )
+              ],
+
+              ),
+            ),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              // gradient: RadialGradient(
+              //     colors:[
+              //       Colors.red,
+              //       Colors.black
+              //     ]
+              // ),
+              image: DecorationImage(
+                image: AssetImage("assets/images/img1.png"),
+                fit: BoxFit.cover  //что типо scylyTypy
+              )
             ),
           ),
           floatingActionButton: FloatingActionButton(
